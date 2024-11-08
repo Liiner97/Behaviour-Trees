14312534adc2e20f761ba5fb58fbb37b70aca9f8
Liiner97 <coelhitohg@gmail.com>
2024-11-08 17:41:07 +0000

    NavMesh Movement

--- Assets/BehaviourTree/RobberBehaviour.cs
+++ Assets/BehaviourTree/RobberBehaviour.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.AI;
 public class RobberBehaviour : MonoBehaviour
@@ -10,7 +12,8 @@ public class RobberBehaviour : MonoBehaviour
     public GameObject van;
     NavMeshAgent agent;
     
-    
+    public enum ActionState { IDLE, WORKING };
+    ActionState state = ActionState.IDLE;
     
     // Start is called before the first frame update
     void Start()
@@ -35,13 +38,32 @@ public class RobberBehaviour : MonoBehaviour
 
     public Node.Status GoToDiamond()
     {
-        agent.SetDestination(diamond.transform.position);
-        return Node.Status.SUCCESS;
+        return GoToLocation(diamond.transform.position);
     }
     public Node.Status GoToVan()
     {
-        agent.SetDestination(van.transform.position);
-        return Node.Status.SUCCESS;
+        return GoToLocation(van.transform.position);
+    }
+
+    Node.Status GoToLocation(Vector3 destination)
+    {
+        float distanceToTarget = Vector3.Distance(destination, this.transform.position);
+        if(state == ActionState.IDLE)
+        {
+            agent.SetDestination(destination);
+            state = ActionState.WORKING;
+        }
+        else if (Vector3.Distance(agent.pathEndPosition, destination)  >= 2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.FAILURE;
+        }
+        else if (distanceToTarget < 2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.SUCCESS;
+        }
+        return Node.Status.RUNNING;
     }
     // Update is called once per frame
     void Update()
